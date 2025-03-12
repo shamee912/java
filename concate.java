bacfae9971aed692df1f6e23aee7cd86834f3d29
shamee912 <shameehasha06@gmail.com>
2025-03-12 15:33:50 +0530

    the full name is

---
+++ concate.java
@@ -0,0 +1,10 @@
+public class concate
+{
+    public static void main(String[] args)
+    {
+      String firstname="shamee";
+      String lastname="sha";
+      String fullname=firstname+" "+lastname;
+      System.out.println("fullname:"+fullname);
+    }
+}
