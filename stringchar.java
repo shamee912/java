608ec4bfc24fe472b3449372e684f2c50da723f3
shamee912 <shameehasha06@gmail.com>
2025-03-13 14:13:01 +0530

    string length

---
+++ stringchar.java
@@ -0,0 +1,9 @@
+public class stringchar
+{
+    public static void main(String[] args)
+    {
+        String str="apple";
+        int length=str.length();
+        System.out.println("The length of the string is:"+length);
+    }
+}
