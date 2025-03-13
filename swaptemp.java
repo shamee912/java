446f5cc0c04302917bc23ac1c65d1394396bb632
shamee912 <shameehasha06@gmail.com>
2025-03-13 13:53:21 +0530

    swaptemp

---
+++ swaptemp.java
@@ -0,0 +1,15 @@
+public class swaptemp
+{
+    public static void main(String[] args)
+    {
+        int a=5;
+        int b=6;
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping:");
+        System.out.println("a:"+a);
+        System.out.println("b:"+b);
+    }
+}
