7d6306df1e7fb43debb4c8f71e78331773a78ec3
shamee912 <shameehasha06@gmail.com>
2025-03-13 11:41:50 +0530

    comparison

---
+++ comparison.java
@@ -0,0 +1,17 @@
+public class comparison
+{
+    public static void main(String[] args)
+    {
+        String first="Shamee";
+        String second="Subi";
+        if(first==second)
+        {
+            System.out.println("they are equal");
+        
+        }
+        else
+        {
+            System.out.println("They are not equal");
+        }
+    }
+}
