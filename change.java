3c2006f9d85da32de6e7e07e5d4c9a67eee67658
shamee912 <shameehasha06@gmail.com>
2025-03-13 10:34:06 +0530

    double

---
+++ change.java
@@ -0,0 +1,10 @@
+public class change
+{
+    public static void main(String[] args)
+    {
+        double mydouble=0.573;
+        int myint=(int)mydouble;
+        System.out.println(myint);
+    
+    }
+}
