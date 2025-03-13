5d1f4693bef51077a17ea29b173e3833edcfe6d4
shamee912 <shameehasha06@gmail.com>
2025-03-13 11:08:02 +0530

    arithmetic operation

---
+++ arithmetic.java
@@ -0,0 +1,12 @@
+public class arithmetic
+{
+    public static void main(String[] args)
+    {
+        int x=10;
+        int y=8;
+        int z=x+y;
+        int a=x-y;
+        System.out.println(z);
+        System.out.println(a);
+    }
+}
