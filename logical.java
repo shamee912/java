33b85983a7e81038e769d106c3826ecb7ab71aec
shamee912 <shameehasha06@gmail.com>
2025-03-13 11:57:55 +0530

    logical operation

---
+++ logical.java
@@ -0,0 +1,17 @@
+public class logical
+{
+    public static void main(String[] args)
+    {
+        int a=3;
+        int b=5;
+        if(a<b && a!=b)
+        {
+            System.out.println("True");
+        }
+        else
+        {
+            System.out.println("False");
+        }
+        
+    }
+}
