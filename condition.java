0e2eb24a8b6fc2171f796eaee8fe24b2bb7d6206
shamee912 <shameehasha06@gmail.com>
2025-03-13 14:37:11 +0530

    condition

---
+++ condition.java
@@ -0,0 +1,17 @@
+public class condition
+{
+    public static void main(String[] args)
+    {
+        int age=20;
+        if(age>=18)
+        {
+          System.out.println("ABle to vote");
+        
+        }
+        else
+        {
+            System.out.println("Not able to vote");
+        }
+        
+    }
+}
