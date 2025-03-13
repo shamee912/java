7b365669717f30f39df10700c091af419b11e136
shamee912 <shameehasha06@gmail.com>
2025-03-13 09:49:16 +0530

    the students detail

---
+++ students.java
@@ -0,0 +1,15 @@
+public class students
+{
+    public static void main(String[] args)
+    {
+    String name="Shameeha";
+    String subname="Computer Science";
+    int mark=90;
+    char grade='A';
+    System.out.println(name);
+    System.out.println(subname);
+    System.out.println(mark);
+    System.out.println(grade);
+    }
+
+}
