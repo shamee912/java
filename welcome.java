5752785612683731f51662099fad7bb7bdf9d74c
shamee912 <shameehasha06@gmail.com>
2025-03-13 10:23:57 +0530

    typecasting

---
+++ welcome.java
@@ -0,0 +1,10 @@
+public class welcome
+{
+    public static void main(String[] args)
+    {
+        int mynum=9;
+        double mydouble=mynum;
+        System.out.println(mydouble);
+    
+    }
+}
