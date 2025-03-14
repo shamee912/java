07a42ea9922c8121ed5a4bd08d5a0e09f0a57ef5
shamee912 <shameehasha06@gmail.com>
2025-03-14 13:54:13 +0530

    array

---
+++ arrays.java
@@ -0,0 +1,13 @@
+public class arrays
+{
+     public static void main(String[] args)
+{
+    String[] fruits={"apple","orange","banana","guava","strawberry"};
+    System.out.println(fruits[0]);
+    System.out.println(fruits[1]);
+    System.out.println(fruits[2]);
+    System.out.println(fruits[3]);
+    System.out.println(fruits[4]);
+    System.out.println("THe length is :"+fruits.length);
+}
+}
