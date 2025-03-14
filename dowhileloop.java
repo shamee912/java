8167500992496c79b27187bf839adbedbe970804
shamee912 <shameehasha06@gmail.com>
2025-03-14 10:56:46 +0530

    0 dowhile

--- dowhileloop.java
+++ dowhileloop.java
@@ -2,14 +2,14 @@ public class dowhileloop
 {
     public static void main(String[] args)
     {
-        String str="apple";
+        
         int i=0;
         do
         {
-            System.out.println(str);
+            System.out.println(i);
             i++;
             
         }
-        while(i<5);
+        while(i>5);
     }
 }
