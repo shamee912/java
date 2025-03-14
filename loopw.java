b86f01e54675f04f4ae9c6dc17f7ff41fd0aee22
shamee912 <shameehasha06@gmail.com>
2025-03-14 10:35:26 +0530

    10 times

--- loopw.java
+++ loopw.java
@@ -3,9 +3,9 @@ public class loopw
     public static void main(String[] args)
     {
         int i=0;
-        while(i<=10)
+        while(i<10)
         {
-            System.out.println(i);
+            System.out.println(10);
             i++;
         }
     }
