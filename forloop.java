df0d1ab9272743a03fb5e34e1c805d925164438b
shamee912 <shameehasha06@gmail.com>
2025-03-14 11:08:17 +0530

    for

---
+++ forloop.java
@@ -0,0 +1,12 @@
+public class forloop
+{
+    public static void main(String[] args)
+    {
+       String str="apple"; 
+        int i;
+        for(i=0;i<10;i++)
+        {
+            System.out.println(str);
+        }
+    }
+}
