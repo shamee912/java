264a314c598cbb2a1cc6827f2c37c20eb29258e4
shamee912 <shameehasha06@gmail.com>
2025-03-14 13:23:29 +0530

    methodsname

--- methods.java
+++ methods.java
@@ -4,10 +4,16 @@ public class methods
   {
         return a * b;
     }
-
+    public static int add(int a, int b)
+  {
+        return a + b;
+    }
     public static void main(String[] args)
      {
-     int result = multiply(6, 7);
+        int result = multiply(6, 7);
         System.out.println("The multiplication of 6 and 7 is: " + result);
+        int result1 = add(6, 7);
+        System.out.println("The addition of 6 and 7 is: " + result1);
     }
+
 }
