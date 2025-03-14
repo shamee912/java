4d0cc4a96890669e17040a2b68a3b41a31527472
shamee912 <shameehasha06@gmail.com>
2025-03-14 14:18:48 +0530

    threed

---
+++ threed.java
@@ -0,0 +1,10 @@
+public class threed 
+{
+    public static void main(String[] args)
+    {
+    int[][][] numbers={{{1,2,3},{1,2,3},{4,5,6}}};
+    System.out.println(numbers[0][0][0]);
+    System.out.println(numbers[0][1][0]);
+    System.out.println(numbers[0][2][1]);
+    }
+}
