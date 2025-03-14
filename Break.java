f5c0b94e828718b976098d677f13011b26225042
shamee912 <shameehasha06@gmail.com>
2025-03-14 11:42:53 +0530

    break

---
+++ Break.java
@@ -0,0 +1,16 @@
+public class Break
+{
+    public static void main(String[] args)
+    {
+        
+        int i;
+        for(i=0;i<10;i++)
+        {
+            System.out.println(i);
+        if(i==7)
+        {
+            break;
+        }
+        }
+    }
+}
