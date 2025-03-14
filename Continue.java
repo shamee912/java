8cbc65095315f35f44b414f424a7de8b681512f0
shamee912 <shameehasha06@gmail.com>
2025-03-14 11:45:32 +0530

    continous

---
+++ Continue.java
@@ -0,0 +1,16 @@
+public class Continue
+{
+    public static void main(String[] args)
+    {
+        
+        int i;
+        for(i=0;i<=10;i++)
+        {
+            System.out.println(i);
+        if(i==7)
+        {
+            continue;
+        }
+        }
+    }
+}
