8c9ca653ceeca25954c47cf4876c1498db07b698
shamee912 <shameehasha06@gmail.com>
2025-03-14 11:09:03 +0530

    forloop

--- forloop.java
+++ forloop.java
@@ -2,11 +2,11 @@ public class forloop
 {
     public static void main(String[] args)
     {
-       String str="apple"; 
+        
         int i;
         for(i=0;i<10;i++)
         {
-            System.out.println(str);
+            System.out.println(i);
         }
     }
 }
