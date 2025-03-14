af2037cdd8382dd897f0b57e55c09691bf5eccc3
shamee912 <shameehasha06@gmail.com>
2025-03-14 10:22:28 +0530

    vowel

---
+++ vowel.java
@@ -0,0 +1,15 @@
+public class vowel
+{
+    public static void main(String[] args)
+    {
+        char ch='R';
+        if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+        {
+            System.out.println("Vowel");
+        }
+        else
+        {
+            System.out.println("Not a vowel"); 
+        }
+    }
+}
