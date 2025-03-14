d336123f24ffa0a46fee379ea5a6dbc25894a251
shamee912 <shameehasha06@gmail.com>
2025-03-14 13:17:43 +0530

    methods

---
+++ methods.java
@@ -0,0 +1,13 @@
+public class methods
+ {
+ public static int multiply(int a, int b)
+  {
+        return a * b;
+    }
+
+    public static void main(String[] args)
+     {
+     int result = multiply(6, 7);
+        System.out.println("The multiplication of 6 and 7 is: " + result);
+    }
+}
