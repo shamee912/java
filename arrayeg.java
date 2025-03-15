cedc89f92c5392f349618c137ffc5c53a15b43e3
shamee912 <shameehasha06@gmail.com>
2025-03-15 10:53:23 +0530

    array eg

---
+++ arrayeg.java
@@ -0,0 +1,12 @@
+public class arrayeg
+{
+    public static void main(String[] args)
+    {
+        int[] price={100,200,300,400,500};
+        for (int i=0;i<=price.length;i++)
+        {
+            System.out.println("Price of product:"+price[i]);
+        }
+
+    }
+}
