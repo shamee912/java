3f46e6f3a7789f1800c5ca4c021c8a3988d646c5
shamee912 <shameehasha06@gmail.com>
2025-03-15 10:33:29 +0530

    billling

---
+++ eg.java
@@ -0,0 +1,12 @@
+public class eg
+{
+    public static void main(String[] args)
+    {
+        
+        int[] arr={1111,2222,3333,4444,5555};
+        for(int i=0;i<arr.length;i++)
+        {
+           System.out.println("Invoice number:"+arr[i]);
+        }
+    }
+}
