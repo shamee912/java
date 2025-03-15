7257489c3b6ea39f3c75b3d4145492ed51f6dd23
shamee912 <shameehasha06@gmail.com>
2025-03-15 09:38:28 +0530

    classes

---
+++ classes.java
@@ -0,0 +1,16 @@
+class student
+{
+    int id;
+    String name;
+    
+}
+public class classes
+{
+    public static void main(String[] args)
+    {
+        student s1=new student();
+        s1.id=101;
+        System.out.println(s1.id);
+    }
+}
+
